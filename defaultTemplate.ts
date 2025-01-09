62255dc16f4525a191184d5f10548e60f7d196b8
yglow <zazaw253@gmail.com>
2025-01-09 10:25:18 -0500

    first commit

---
+++ defaultTemplate.ts
@@ -0,0 +1,73 @@
+export const defaultTemplate = `---
+tags:
+  - "#people"
+---
+
+# Table of Content
+\`\`\`table-of-contents
+\`\`\`
+
+
+# Basic Information
+- Names:
+- Birthday: 
+- Age:
+- Gender
+
+# Relationship Dynamics
+- Role in My Life:
+- Shared Interests:
+- Preferred Activities Together:
+- How We Met:
+- Gift Ideas:
+- Love Languages:
+
+# Cultural Background
+- Origin:
+- Ethnic Heritage:
+- Religious/Spiritual Affiliation:
+- Languages:
+- Key Traditions:
+- Core Values:
+- Gender Role Perspectives:
+- Time Management Approach:
+
+# Personality Traits
+- Communication Style:
+- Emotional tendencies:
+
+# Social Connections
+- Family Structure:
+- Romantic Relationships:
+- Friend Circle:
+- Other Significant Relationships:
+
+# Professional Life
+- Current Occupation:
+- Past Occupations:
+- Employer(s):
+
+# Interests and Hobbies
+- Entertainment Preferences:
+- Fitness Activities:
+- Pets/Animals:
+- Travel Interests:
+- Other Hobbies:
+
+# Preferences
+- Likes:
+- Dislikes:
+
+# Personal History
+- Significant Life Events:
+- Recent Developments:
+
+# Future Aspirations and Goals
+- Future Aspirations:
+- Short-term Goals:
+# Additional Notes
+
+# Interaction Log
+- Date | Event | Notes
+
+`;
--- main.ts
+++ main.ts
@@ -1,134 +1,126 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, MarkdownView, Notice, TFile, normalizePath } from 'obsidian';
+import { defaultTemplate } from './defaultTemplate';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+// Define the settings interface
+interface PersonNotePluginSettings {
+    templatePath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+// Define the default settings
+const DEFAULT_SETTINGS: PersonNotePluginSettings = {
+    templatePath: ''
+};
+
+// Define the main plugin class
+export default class PersonNotePlugin extends Plugin {
+    settings: PersonNotePluginSettings;
+
+    async onload() {
+        console.log('PersonNotePlugin loaded');
+
+        // Load settings
+        await this.loadSettings();
+
+        // Add a command to the Obsidian command palette
+        this.addCommand({
+            id: 'add-person-note',
+            name: 'Add Person Note',
+            callback: () => {
+                console.log('Add Person Note command executed');
+                this.addPersonNote();
+            }
+        });
+
+        // Add settings tab
+        this.addSettingTab(new PersonNoteSettingTab(this.app, this));
+    }
+
+    // Method to create a new person note
+    async addPersonNote() {
+        console.log('addPersonNote method called');
+
+        // Prompt the user for the file name
+        const fileName = await this.promptForFileName();
+        if (!fileName) {
+            console.log('No file name provided');
+            return;
+        }
+
+        let template = defaultTemplate;
+
+        // Read the template from the configured .md file if the path is not empty
+        if (this.settings.templatePath) {
+            const templateFilePath = normalizePath(this.settings.templatePath);
+            const templateFile = this.app.vault.getAbstractFileByPath(templateFilePath) as TFile;
+            if (!templateFile) {
+                new Notice('Template file not found');
+                return;
+            }
+            template = await this.app.vault.read(templateFile);
+        }
+
+        // Create a new file with the template
+        const file = await this.app.vault.create(`People/${fileName}.md`, template.replace('{{title}}', fileName));
+        console.log(`File created: People/${fileName}.md`);
+        
+        // Open the newly created file using Workspace.getLeaf
+        const leaf = this.app.workspace.getLeaf();
+        await leaf.openFile(file);
+        console.log(`File opened: People/${fileName}.md`);
+    }
+
+    // Method to prompt the user for the file name
+    async promptForFileName(): Promise<string | null> {
+        return new Promise((resolve) => {
+            const prompt = new Notice('Enter the file name:', 0);
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.placeholder = 'File name';
+            input.addEventListener('keydown', (event) => {
+                if (event.key === 'Enter') {
+                    prompt.hide();
+                    resolve(input.value);
+                }
+            });
+            prompt.noticeEl.appendChild(input);
+            input.focus();
+        });
+    }
+
+    // Load settings
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    // Save settings
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+// Define the settings tab class
+class PersonNoteSettingTab extends PluginSettingTab {
+    plugin: PersonNotePlugin;
 
-	async onload() {
-		await this.loadSettings();
+    constructor(app: App, plugin: PersonNotePlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    display(): void {
+        const { containerEl } = this;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        containerEl.empty();
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Template Path')
+            .setDesc('Path to the template file for person notes')
+            .addText(text => text
+                .setPlaceholder('Templates/PersonNoteTemplate.md')
+                .setValue(this.plugin.settings.templatePath)
+                .onChange(async (value) => {
+                    this.plugin.settings.templatePath = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
